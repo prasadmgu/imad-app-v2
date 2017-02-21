930494313f1b3b6990da8a69881dce11d2611846
prasadmgu <prasadmgu@gmail.com>
2017-02-21 20:01:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res){
-    res.send('Article one is requested and will be served here');
-    app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req,res){
     res.send('Article two is requested and will be served here');
