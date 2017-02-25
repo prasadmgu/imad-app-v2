f14eff5ca84844101ea91ccbf543e8fe4f4a2d88
prasadmgu <prasadmgu@gmail.com>
2017-02-25 20:50:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,76 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+            var express = require('express');
+            var morgan = require('morgan');
+            var path = require('path');
+            
+            var app = express();
+            app.use(morgan('combined'));
+            
+            app.get('/', function (req, res) {
+            res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+            });
+            
+            var article one ={
+            title:'Article one | Prasad K G',
+            heading:'Article One',
+            date: 'Feb 21, 2017',
+            content:`
+                  <p>
+                     This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article.This is first article. This is first article.
+                  </p>
+                   <p>
+                     This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article.This is first article. This is first article.
+                  </p>
+                   <p>
+                     This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article. This is first article.This is first article. This is first article.
+                  </p>
+            `
+            };
 
-var app = express();
-app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+function createTemplate(data){
+    var title=data.title;
+    var head = data.head;
+    var date = data.date;
+    var content=data.content;
+    
 
+ var htmlTemplate= `
+            <html>
+            <head>
+            <title>
+              {title}
+            </title>
+            
+            <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            
+            
+            <body>
+            <div class="container"> 
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                {heading}
+              </h3>
+              <div>
+                  Feb 21,2017
+              </div>
+                
+                  <div>
+                  {content}
+                  </div>
+             </div>
+            </body>
+            </html>
+            
+`
+;   
+return htmlTemplate;
+}
 app.get('/article-one', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleone));
 });
 app.get('/article-two', function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
