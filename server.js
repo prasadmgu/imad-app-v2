051f17a1b847143a04a44bd69bae660c78e5da54
prasadmgu <prasadmgu@gmail.com>
2017-02-25 21:50:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ function createTemplate(data){
 ;   
 return htmlTemplate;
 }
-app.get('/.articleName', function (req,res){
+app.get('/:articleName', function (req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
