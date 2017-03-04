9ca9ed5bb7bb79fde28da5d57e3d8dee2ed39d6c
prasadmgu <prasadmgu@gmail.com>
2017-03-04 20:51:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element=document.getElementById('main-text');
 element.innerHTML='new value';
 // move the image
 var img=document.getElementById('madi');
-img.onClick=function(){
+img.onclick=function() {
   img.style.marginLeft='100px'  ;
 };
